f57714f584beed8fa99b04aaf802ce3a5a39de97
Chikezie Ndubuisi <chikeziendubuisi7@gmail.com>
2025-11-24 17:05:15 +0100

    Update navigation positioning and mobile hero title: push navigation to far right on desktop, change mobile hero title to 'The Future of Global Payments and Financial Infrastructure'

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -21,8 +21,9 @@ const Home = () => {
               <span className="highlight-text">Infrastructure</span>
             </h1>
             <h1 className="hero-title-mobile">
-              Global payment you can<br />
-              actually trust
+              The Future of Global<br />
+              Payments and Financial<br />
+              Infrastructure
             </h1>
             <p className="hero-description">
               Built on the XRP Ledger, TrustiChain delivers fast, secure, and fully compliant 
